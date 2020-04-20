fd9985c4b25a4d4dd1a7f064575f0dba6be1b88a
afernandezfontenla <56158212+afernandezfontenla@users.noreply.github.com>
2020-04-20 13:28:29 +0200

    Add files via upload

---
+++ progtarea4/ProgTarea4.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progtarea4;
+
+/**
+ *
+ * @author Nuevo usuario
+ */
+public class ProgTarea4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Ventana ven = new Ventana();
+        ven.setVisible(true);
+        
+    }
+    
+}
